ee6b52c1c1164649362416da745a37e9a32c2ff4
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-06-06 07:47:23 +0000

    [Add] user_index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,27 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def edit
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+    @user = current_user
   end
 
   def show
