f903b0a2f854176e39c4bed3d1bc34d9d7767d9a
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-06-17 08:44:57 +0000

    [Update] ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,8 +1,9 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment = BookComment.new(book_comment_params)
     @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
     @book_comment.save
 
   end
