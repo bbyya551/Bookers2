1229524b3fb5ab151247efd089cf66603f1a98c9
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-01 14:46:17 +0000

    [Add] follow_initial

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_books, through: :favorites, source: :book
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, uniqueness: true,
@@ -27,5 +34,13 @@ class User < ApplicationRecord
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
     end
+  end
+
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
 end
