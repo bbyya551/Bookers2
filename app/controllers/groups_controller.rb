773fe9bd178b51c19cae9f0c159fc7206f2ac5f4
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-10 14:35:05 +0000

    [Add]8c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,12 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to groups_path
+  end
+
   def new
     @group = Group.new
   end
@@ -21,6 +27,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.users << current_user
     if @group.save
       redirect_to groups_path
     else
@@ -29,16 +36,20 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path
     else
       render :edit
     end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.destroy(current_user)
+    redirect_to groups_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :groups, except: [:destroy]
+  resources :groups do
+    get 'join' => 'groups#join'
+  end
 end
