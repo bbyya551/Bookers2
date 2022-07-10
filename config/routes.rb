9e78450ee2b6bd48df5618ec03ff1e769b8af21d
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-10 10:08:01 +0000

    [Add] initial commit 7c

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :groups, except: [:destroy]
 end
