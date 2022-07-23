da20d94212c53884f728a12619fbd93508464a79
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-23 10:23:18 +0000

    [Add] 7d star

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_10_021932) do
+ActiveRecord::Schema.define(version: 2022_07_23_084350) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_07_10_021932) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
+    t.string "star"
   end
 
   create_table "entries", force: :cascade do |t|
