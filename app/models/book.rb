cc106abaa172d98e1a945ba0e6cff9128a4a44e5
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-26 06:26:39 +0000

    [Add] search

--- app/models/book.rb
+++ app/models/book.rb
@@ -42,4 +42,16 @@ class Book < ApplicationRecord
       end
     end
   end
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
+    else
+      Book.where('title LIKE ?', '%'+content+'%')
+    end
+  end
 end
